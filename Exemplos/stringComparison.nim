e11864aa560bb25c0b814be1b4dc5d329345af99
Khalil <khalilmotta@outlook.com>
2024-12-08 15:23:24 -0300

    Update stringComparison.nim

--- Exemplos/stringComparison.nim
+++ Exemplos/stringComparison.nim
@@ -8,8 +8,6 @@ var
     s2 = "axyz"
     s3 = "ba"
     s4 = "ba "
-    s5 = "z"    #var de teste
-    s6 = "za"   #var de teste
 
 echo c1 < c2
 echo c1 < c3
@@ -17,8 +15,3 @@ echo c1 < c3
 echo s1 < s2
 echo s2 < s3
 echo s3 < s4
-
-echo "\nMeus testes:"
-echo s4 < s5
-echo s2 < s5
-echo s5 < s6
